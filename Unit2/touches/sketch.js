b512f773b3600428236f21a9ad75d136090a9ae7
QuietH <hamzazagha@yahoo.com>
2022-10-07 18:00:32 -0500

    Touches

--- Unit2/touches/sketch.js
+++ Unit2/touches/sketch.js
@@ -1,32 +1,40 @@
 let numberOfTouches ;
+let i1, i2, i3, i4 ;
+
 
 function setup() {
   createCanvas(400, 400);
+  i1 = loadImage("assets/car.jfif");
+  i2 = loadImage("assets/car2.png");
+  i3 = loadImage("assets/woopsS.png");
+  i4 = loadImage("assets/pie.png");
+  imageMode(CENTER);
 }
 
 function draw() {
   clear();
   numberOfTouches = touches.length;
-  text(numberOfTouches + ' touches', 5, 10);
+  text(numberOfTouches + ' touches', width/2, 70);
 
   switch(numberOfTouches) {
     case 0: 
-      text("Peace and Tranquility", 5, 22) ; 
+      text("Thank God,\nNo one is here.", width/2, 90) ; 
+      image(i1, width/2, height/2, 100, 100);
       break ;
 
       case 1: 
-       text("Meditation", 5, 22) ; 
-      // put a picture here
+       text("Can you leave\nme alone?", width/2, 90) ; 
+       image(i2, width/2, height/2, 100, 100);
       break ;
 
       case 2:
-      text("Human Connection", 5, 22) ; 
-            // put a picture here
+      text("Please don't\ndo this.", width/2, 90) ; 
+      image(i3, width/2, height/2, 100, 100);
       break ;
 
       case 3:
-     text("There are now too many touches, please minimize the amount.", 5, 22) ; 
-            // put a picture here
+     text("This is the\nworst day of my life.", width/2, 90) ; 
+     image(i4, width/2, height/2, 100, 100);
       break ;
 
 
